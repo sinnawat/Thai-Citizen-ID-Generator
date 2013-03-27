06dd4c5585b2e700b382384e025e95f65124ffd1
Sinnawat <sinnawat@gmail.com>
2013-03-27 10:49:57 +0700

    index file

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+# สุ่มหมายเลขบัตรประชาชน
+function randomCitizenID(){
+	for($i=0;$i<12;$i++){
+		$k = abs($i + (-13));
+		$m = rand(0,9);
+		$firstNumber .= $m; // ตัวเลขชุดแรก (12 หลัก)
+		$numberCalc += ($k * $m);
+	}
+	$lastNumber = 11 - ($numberCalc % 11); // ตัวเลขหลักสุดท้าย
+	return $firstNumber.$lastNumber;
+}
+
+echo randomCitizenID();
+?>
